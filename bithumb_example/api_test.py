15454a8fd78ef39189390d301daa946c8bda8109
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-09-04 23:56:13 +0900

    first commit

--- bithumb_example/api_test.py
+++ bithumb_example/api_test.py
@@ -17,20 +17,19 @@
 # python setup.py install
 
 import sys
-from xcoin_api_client import *
+import os
+from bithumb_example.xcoin_api_client import *
 import pprint
 
+api_key = os.environ.get('API_BITHUMB_CONNECT_KEY')
+api_secret = os.environ.get('API_BITHUMB_SECRET_KEY')
 
-api_key = "api_connect_key";
-api_secret = "api_secret_key";
-
-api = XCoinAPI(api_key, api_secret);
+api = XCoinAPI(api_key, api_secret)
 
 rgParams = {
-	"order_currency" : "BTC",
-	"payment_currency" : "KRW"
-};
-
+    "order_currency": "BTC",
+    "payment_currency": "KRW"
+}
 
 #
 # public api
@@ -40,12 +39,11 @@ rgParams = {
 # /public/orderbook
 # /public/recent_transactions
 
-result = api.xcoinApiCall("/public/ticker", rgParams);
-print("status: " + result["status"]);
-print("last: " + result["data"]["closing_price"]);
-print("sell: " + result["data"]["sell_price"]);
-print("buy: " + result["data"]["buy_price"]);
-
+result = api.xcoinApiCall("/public/ticker", rgParams)
+print("status: " + result["status"])
+print("last: " + result["data"]["closing_price"])
+print("sell: " + result["data"]["sell_price"])
+print("buy: " + result["data"]["buy_price"])
 
 #
 # private api
@@ -56,12 +54,11 @@ print("buy: " + result["data"]["buy_price"]);
 # /info/balance
 # /info/wallet_address
 
-#result = api.xcoinApiCall("/info/account", rgParams);
-#print("status: " + result["status"]);
-#print("created: " + result["data"]["created"]);
-#print("account id: " + result["data"]["account_id"]);
-#print("trade fee: " + result["data"]["trade_fee"]);
-#print("balance: " + result["data"]["balance"]);
+# result = api.xcoinApiCall("/info/account", rgParams);
+# print("status: " + result["status"]);
+# print("created: " + result["data"]["created"]);
+# print("account id: " + result["data"]["account_id"]);
+# print("trade fee: " + result["data"]["trade_fee"]);
+# print("balance: " + result["data"]["balance"]);
 
-sys.exit(0);
-
+sys.exit(0)
